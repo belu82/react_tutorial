9305762882373d63cea009c03362cc5fa7410505
belu82 <44766864+belu82@users.noreply.github.com>
2020-08-18 10:55:42 +0200

    Handling Events

--- React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/StorePicker.js
+++ React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/StorePicker.js
@@ -8,7 +8,8 @@ class StorePicker extends React.Component{
     goToStore = (event) =>{
 
         event.preventDefault();
-        console.log(this);
+        const storeName = (this.myInput.current.value);
+        this.props.history.push(`/store/${storeName}`);
     };
     
     render(){
