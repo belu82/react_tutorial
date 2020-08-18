88a03634ec82ba6f5519a1e74d52b99f12c9f7f0
belu82 <44766864+belu82@users.noreply.github.com>
2020-08-18 17:51:24 +0200

    Add to the order in the cart is ready

--- React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/App.js
+++ React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/App.js
@@ -2,19 +2,54 @@ import React from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
-
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 
 
 class App extends React.Component{
+    state= {
+        fishes: {},
+        order: {}
+    };
+
+    addFish = fish => {
+        //console.log("Adding fishsisihdfihiashsdf");
+        //másolat készítése: 
+        const fishes = {...this.state.fishes};
+        //add new fish to that fishes variable
+        fishes[`fish${Date.now()}`] = fish;
+
+        this.setState({fishes});
+    };
+
+    addToOrder = (key) => {
+        //tak a copy of state
+        const order = {...this.state.order};
+        //either add to the order, or update the number in our order
+        order[key] = order[key] + 1 || 1;
+
+        //setState to update our state object
+        this.setState({order});
+    }
+
+    loadSampleFishes = () => {
+        this.setState({fishes:sampleFishes});
+
+    }
     render(){
         return(
 
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="I'm menyus" age={21} cool={true} />
+                    <ul className="fishes">
+
+                        {Object.keys(this.state.fishes).map(key => < Fish key={key} index={key}details = {this.state.fishes[key]} addToOrder={this.addToOrder}/>)}
+
+                    </ul>
                 </div>
               <Order/>
-              <Inventory/>
+              <Inventory addFish = {this.addFish} loadSampleFishes = {this.loadSampleFishes}/>
             </div>
         );
     }
