1df7e91e821579357d09dec5822bc468ad6ec535
belu82 <44766864+belu82@users.noreply.github.com>
2020-08-18 10:41:25 +0200

    Event, ref and this binding

--- React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/StorePicker.js
+++ React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/StorePicker.js
@@ -1,12 +1,21 @@
 
 import React from 'react';
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component{
+    myInput = React.createRef();
+
+    goToStore = (event) =>{
+
+        event.preventDefault();
+        console.log(this);
+    };
+    
     render(){
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore }>
                 <h2>Please enter the store</h2>
-                <input type="text" required placeholder= "Store name"></input>
+                <input type="text" required placeholder= "Store name" defaultValue={getFunName()} ref={this.myInput}></input>
                 <button type="submit">Visit store</button>
             </form>
 
