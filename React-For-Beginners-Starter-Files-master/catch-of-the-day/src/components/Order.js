659aa4b9a1db01693d76806c3f37d44960f761ab
belu82 <44766864+belu82@users.noreply.github.com>
2020-08-25 11:20:04 +0200

    Order menu is ready. The progam can adding elements to the list and summin the prices

--- React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/Order.js
+++ React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/Order.js
@@ -1,8 +1,42 @@
     import React from 'react';
+    import {formatPrice} from "../helpers";
 
     class Order extends React.Component{
+        renderOrder = (key) => {
+            const fish = this.props.fishes[key];
+            const count = this.props.order[key];
+            const isAvailable = fish.status === "available";
+            if(!isAvailable){
+                return <li key={key}>Sorry {fish ? fish.name : "fish"} is no longer available</li>;
+            }
+            return( 
+            <li key={key}>
+                {count} lbs {fish.name}
+                {formatPrice(count*fish.price)}
+            </li>
+            );
+        };
         render(){
-            return <div className="order">Order</div>
+            const orderId = Object.keys(this.props.order);//iterate trought the state contains
+            const total = orderId.reduce((prevTotal, key) => {
+                const fish = this.props.fishes[key];
+                const count = this.props.order[key];
+                const isAvailable= fish && fish.status === "available";
+                if(isAvailable) {
+                    return prevTotal + (count *fish.price);
+                }
+                return prevTotal;
+            },0);
+
+            //object spread kerülendő
+            return <div className="order-wrap">
+                <h2>Order</h2>
+                <ul className="order">{orderId.map(/*key => <li>{key}</li>)*/ this.renderOrder)}</ul>
+                <div className="total">
+                    Total: 
+                    <strong>{formatPrice(total)}</strong>
+                </div>
+            </div>
         }
     }
 
